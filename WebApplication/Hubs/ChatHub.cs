03885b18b596565a227977ff432baabc1f6c0246
ClarenceKuo <mkuo8@wisc.edu>
2019-08-08 11:46:35 +0800

    Add Kafka Producer
    Triggered and initiated by calling UploadMessage() (in SignalR), producer will broadcast the input string to all topic selected.

--- WebApplication/Hubs/ChatHub.cs
+++ WebApplication/Hubs/ChatHub.cs
@@ -18,6 +18,25 @@ namespace SignalRChat.Hubs
                 await Clients.Caller.SendAsync("ReceiveMessage", message);
             }
         }
-        
+
+         public async Task UploadMessage(List<Chat> chats)
+        {
+            var config = new ProducerConfig { BootstrapServers = Constant.BrokerIP };
+            foreach (Chat chat in chats)
+            {
+                using (var producer = new ProducerBuilder<string, string>(config).Build())
+                {
+                    try
+                    {
+                        var deliveryReport = await producer.ProduceAsync(
+                            chat.Topic, new Message<string, string> { Key = chat.Id, Value = JsonConvert.SerializeObject(chat.Content)});
+                    }
+                    catch (ProduceException<string, string> e)
+                    {
+                        Console.WriteLine($"failed to deliver message: {e.Message} [{e.Error.Code}]");
+                    }
+                }
+            }
+        }
     }
 }
